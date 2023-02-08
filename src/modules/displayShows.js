92f791c851c3c05dafc0215eaef58ab6b8e412b3
elhamatokhi <tokhielhama30@gmail.com>
2023-02-08 14:46:58 +0430

    Add popup to comment button

--- src/modules/displayShows.js
+++ src/modules/displayShows.js
@@ -1,10 +1,11 @@
-import fetchData from './fetchData.js';
+import fetchData from "./fetchData.js";
+import { getSingleData } from "./popup.js";
 
-const showList = document.querySelector('.shows-list');
+const showList = document.querySelector(".shows-list");
 
 const displayAllShows = async () => {
   const getData = await fetchData();
-  showList.innerHTML = '';
+  showList.innerHTML = "";
   getData.forEach((data) => {
     showList.innerHTML += `
       <div class='card'>
@@ -13,10 +14,20 @@ const displayAllShows = async () => {
           <p class='show-name'>${data.name}</p>
           <p class='heart-button'></p>
         </div>
-        <button type='button' class='buttons'>Comments</button>
+        <button type='button' class='buttons btn-comment' data-id=${data.id}>Comments</button>
         <button type='button' class='buttons'>Reservations</button>
       </div>
     `;
+  });
+
+  const popup = document.querySelector(".popup");
+  const btnComment = document.querySelectorAll(".btn-comment");
+  Array.from(btnComment)?.forEach((button) => {
+    button.addEventListener("click", () => {
+      const { id } = button.dataset;
+      getSingleData(id);
+      popup.classList.add("show");
+    });
   });
 };
 
