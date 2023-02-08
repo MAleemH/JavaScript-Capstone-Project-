356280fc235c656910230d670b6006f110bfd625
elhamatokhi <tokhielhama30@gmail.com>
2023-02-08 14:56:42 +0430

    Fix linters errors

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,25 +1,24 @@
-import fetchData from "./fetchData.js";
+import fetchData from './fetchData.js';
 
-export const getSingleData = async (id) => {
+const getSingleData = async (id) => {
   const getData = await fetchData();
-  const singleData = getData.find((data) => data.id === parseInt(id));
-  console.log(singleData);
+  const singleData = getData.find((data) => data.id === parseInt(id, 10));
+  const popup = document.querySelector('.popup');
+  const popupContent = document.querySelector('.popup-content');
+  const popupClose = document.querySelector('.popup-close');
 
-  const popup = document.querySelector(".popup");
-  const popupContent = document.querySelector(".popup-content");
-  const popupClose = document.querySelector(".popup-close");
-
-  popupContent.innerHTML = 
-  `<img src='${singleData.image.original}' alt='Thumbnail' class='img-card' >
+  popupContent.innerHTML = `<img src='${singleData.image.original}' alt='Thumbnail' class='img-card' >
       <div class='popup-info'>
           <h1>${singleData?.name}</h1>
           <p><b>Genre(s):</b> ${singleData?.genres}</p>
           <p class="summary"><b>Summary:</b> ${singleData?.summary}</p>
       </div>
   `;
-  popupClose.addEventListener("click", () => {
-    popup.classList.remove("show");
+  popupClose.addEventListener('click', () => {
+    popup.classList.remove('show');
   });
 
   popup.append(popupContent, popupClose);
 };
+
+export default getSingleData;
