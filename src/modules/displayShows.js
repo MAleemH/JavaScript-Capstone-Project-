ddb8a7d42435ebad45ba11336f143eca0c6226f0
Your Name <you@example.com>
2023-02-08 13:28:48 +0500

    involvementAPI created with unique id and added in js file

--- src/modules/displayShows.js
+++ src/modules/displayShows.js
@@ -4,6 +4,7 @@ const showList = document.querySelector('.shows-list');
 
 const displayAllShows = async () => {
   const getData = await fetchData();
+  // console.log(getData);
   showList.innerHTML = '';
   getData.forEach((data) => {
     showList.innerHTML += `
@@ -11,7 +12,7 @@ const displayAllShows = async () => {
         <img src='${data.image.medium}' alt='Thumbnail' >
         <div class='title-like'>
           <p class='show-name'>${data.name}</p>
-          <p class='heart-button'></p>
+          <i class="fa-regular fa-heart"></i>
         </div>
         <button type='button' class='buttons'>Comments</button>
         <button type='button' class='buttons'>Reservations</button>
