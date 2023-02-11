f84204097362e3f8159558d7219ab0ec00562a7a
elhamatokhi <tokhielhama30@gmail.com>
2023-02-11 15:20:15 +0430

    Fix eslint errors

--- src/modules/commentCounter.js
+++ src/modules/commentCounter.js
@@ -1,5 +1,5 @@
 const commentCounter = () => {
-  const singleComment = document.querySelectorAll(".addedComments");
+  const singleComment = document.querySelectorAll('.addedComments');
   return singleComment.length;
 };
 export default commentCounter;
--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,10 +1,10 @@
-import commentsData from "./fetchComments";
-import commentCounter from "./commentCounter";
+import commentsData from './fetchComments';
+import commentCounter from './commentCounter';
 
 const updateComment = async (id) => {
   const getCommentsData = await commentsData(id);
-  const comments = document.querySelector(".comments");
-  const showComments = document.querySelector(".showComments");
+  const comments = document.querySelector('.comments');
+  const showComments = document.querySelector('.showComments');
   getCommentsData.forEach((comment) => {
     showComments.innerHTML += `
     <p class="addedComments">${comment.creation_date} ${comment.username}: ${comment.comment}</p>
@@ -12,23 +12,23 @@ const updateComment = async (id) => {
   });
   comments.appendChild(showComments);
 
-  //Add comment counter
+  // Add comment counter
   const numOfComments = commentCounter();
-  const commentTitle = document.querySelector("h2");
+  const commentTitle = document.querySelector('h2');
   commentTitle.innerHTML = `
     Comments(${numOfComments}) `;
-  const commentsHTML = document.querySelector(".commentsHTML");
-  commentsHTML.classList.add("displayNone");
+  const commentsHTML = document.querySelector('.commentsHTML');
+  commentsHTML.classList.add('displayNone');
 };
 
 export const addUi = (username, comment) => {
-  const comments = document.querySelector(".comments");
-  const showComments = document.querySelector(".showComments");
+  const comments = document.querySelector('.comments');
+  const showComments = document.querySelector('.showComments');
 
   const date = new Date();
   const dateString = `${date.getFullYear()}-${(date.getMonth() + 1)
     .toString()
-    .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+    .padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`;
   showComments.innerHTML += `
   <p class="addedComments">${dateString} ${username}: ${comment}</p>
   `;
