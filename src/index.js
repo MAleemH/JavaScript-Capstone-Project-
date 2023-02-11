c02146da1aa6fa5db63b19177c7e1901a02f36c7
MAleemH <aleemhashmi4321@gmail.com>
2023-02-11 10:17:56 +0500

    added call action for like

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import './style.css';
-import displayAllShows from './modules/displayShows';
+import displayAllShows from './modules/displayShows.js';
+import addNewLike from './modules/addNewLike.js';
 
 displayAllShows();
+
+// add new like
+window.addEventListener('click', async (e) => {
+  const likeB = e.target;
+  if (likeB.classList.contains('likeButton')) {
+    const showId = likeB.getAttribute('data-id');
+    await addNewLike(showId);
+    window.location.reload();
+  }
+})
