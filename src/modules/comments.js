99b7649ef33fe9522b888265ebede858cec0824e
elhamatokhi <tokhielhama30@gmail.com>
2023-02-11 15:18:35 +0430

    Add comment counter function to count comments

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,9 +1,10 @@
-import commentsData from './fetchComments';
+import commentsData from "./fetchComments";
+import commentCounter from "./commentCounter";
 
 const updateComment = async (id) => {
   const getCommentsData = await commentsData(id);
-  const comments = document.querySelector('.comments');
-  const showComments = document.querySelector('.showComments');
+  const comments = document.querySelector(".comments");
+  const showComments = document.querySelector(".showComments");
   getCommentsData.forEach((comment) => {
     showComments.innerHTML += `
     <p class="addedComments">${comment.creation_date} ${comment.username}: ${comment.comment}</p>
@@ -11,23 +12,23 @@ const updateComment = async (id) => {
   });
   comments.appendChild(showComments);
 
-  const commentTitle = document.querySelector('h2');
+  //Add comment counter
+  const numOfComments = commentCounter();
+  const commentTitle = document.querySelector("h2");
   commentTitle.innerHTML = `
-    Comments `;
-  const commentsHTML = document.querySelector('.commentsHTML');
-  commentsHTML.classList.add('displayNone');
+    Comments(${numOfComments}) `;
+  const commentsHTML = document.querySelector(".commentsHTML");
+  commentsHTML.classList.add("displayNone");
 };
 
 export const addUi = (username, comment) => {
-  const comments = document.querySelector('.comments');
-  const showComments = document.querySelector('.showComments');
+  const comments = document.querySelector(".comments");
+  const showComments = document.querySelector(".showComments");
 
   const date = new Date();
-  const dateString = `${date.getFullYear()
-  }-${
-    (date.getMonth() + 1).toString().padStart(2, '0')
-  }-${
-    date.getDate().toString().padStart(2, '0')}`;
+  const dateString = `${date.getFullYear()}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
   showComments.innerHTML += `
   <p class="addedComments">${dateString} ${username}: ${comment}</p>
   `;
