11124108be509131aa3fd5b8b474a5c621234207
MAleemH <aleemhashmi4321@gmail.com>
2023-02-11 15:03:09 +0500

    removed navbar.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import './style.css';
 import displayAllShows from './modules/displayShows';
 import addNewLike from './modules/addNewLike';
-import displayNavbar from './modules/navbar';
 
-displayNavbar();
 displayAllShows();
 
 // add new like
